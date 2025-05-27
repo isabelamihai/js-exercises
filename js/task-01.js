a4daa06ab7791d221bdf8c17fe881e75db7645f7
isabelamihai <isabelaelenamihai@gmail.com>
2025-05-27 18:52:53 +0300

    Update task 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,6 +1,8 @@
-console.log("Number of categories:", document.getElementsByTagName("h2").length);
 const categories = document.querySelectorAll(".item");
-categories.forEach((category) => {  
-  console.log("Category:", category.querySelector("h2").textContent);
-  console.log("Elements:", category.querySelectorAll("li").length);
-});  
+console.log("Number of categories:", categories.length);
+categories.forEach((category) => {
+  const categoryName = category.querySelector("h2").textContent;
+  const elementsCount = category.querySelectorAll("li").length;
+  console.log(`Category: ${categoryName}`);
+  console.log(`Elements: ${elementsCount}`);
+});
