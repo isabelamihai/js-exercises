4fbbc346c59f870f0823c4d65b9b9a7b66445224
isabelamihai <isabelaelenamihai@gmail.com>
2025-05-27 21:45:21 +0300

    Update task 2

--- js/task-01.js
+++ js/task-01.js
@@ -1,8 +1,10 @@
-const categories = document.querySelectorAll(".item");
-console.log("Number of categories:", categories.length);
-categories.forEach((category) => {
-  const categoryName = category.querySelector("h2").textContent;
-  const elementsCount = category.querySelectorAll("li").length;
-  console.log(`Category: ${categoryName}`);
-  console.log(`Elements: ${elementsCount}`);
+const categoryList = document.querySelectorAll('.item');
+console.log("Number of categories:", categoryList.length);
+
+
+categoryList.forEach(category => {
+    const header = category.querySelector('h2');
+    const elements = category.querySelectorAll('li');
+    console.log("Category:",header.textContent);
+    console.log("Elements:", elements.length);
 });
--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+  const list = document.createElement('ul');
+ingredients.forEach(ingredient => {
+  const item = document.createElement('li');
+  item.textContent = ingredient;
+  item.classList.add('item');
+  list.append(item);
+});
+console.log(list);
+document.body.append(list);
